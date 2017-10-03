fa8c8be6b7e0bded051df57a233bcd0e65b591a1
leesc22 <leesiewching22@gmail.com>
2017-10-03 10:49:27 +0800

    complete first challenge - extract variable

--- extract_variable.rb
+++ extract_variable.rb
@@ -1,9 +1,22 @@
 # extract_variable.rb
 #https://refactoring.com/catalog/extractVariable.html
 
+def length_more_than_three(string)
+	string.length > 3
+end
+
+def length_less_than_eight(string)
+	string.length <= 7
+end
+
+def starts_with_vowel(string)
+	string.downcase.start_with?("a", "e", "i", "o", "u")
+end
+
+
 username = "Alice"
 
-if ( username.length > 3 && username.length <= 7 ) || ( username.downcase.start_with?("a", "e", "i", "o", "u") )
+if ( length_more_than_three(username) && length_less_than_eight(username) || starts_with_vowel(username) )
 	puts "Congratulations #{username}! You won 1 million dollars!"
 else
 	puts "Please try again!"
