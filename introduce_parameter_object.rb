fc334f6088b36ab7d0b85d266559b57d1e19aa28
leesc22 <leesiewching22@gmail.com>
2017-10-23 09:39:25 +0800

    first refactoring by introduce parameter object

--- introduce_parameter_object.rb
+++ introduce_parameter_object.rb
@@ -1,6 +1,9 @@
 # introduce_parameter_object.rb
 #http://refactoring.com/catalog/introduceParameterObject.html
 
+# Replace Parameter with Method Call
+# http://ieftimov.com/refactoring-smelly-parameters-lists
+
 class GamePackageCalculator
 
 	BASIC_PACKAGE_FACTOR = 1.0
@@ -12,30 +15,44 @@ class GamePackageCalculator
 
 
 	#calculates the cost of a gamer who wants to get X gold, Y powerup and number of lives derived from X gold and Y powerup. 
-	def calculate_cost(gold, powerups, premium = nil)
-
-		lives = calculate_lives(gold, powerups) 
+	def calculate_cost(package, premium = nil)
+		lives = calculate_lives(package) 
 		
 		if premium
-			premium_cost(lives, gold, powerups) 
+			premium_cost(lives, package) 
 		else
-			basic_cost(lives, gold, powerups)
+			basic_cost(lives, package)
 		end
 	end	
 
 	private 
 
-	def calculate_lives(gold, powerups)
-		(gold * GOLD_LIFE_RATIO ).round(0) + (powerups * POWERUPS_LIFE_RATIO).round(0) 
+	def calculate_lives(package)
+		(package.gold * GOLD_LIFE_RATIO ).round(0) + (package.powerups * POWERUPS_LIFE_RATIO).round(0) 
 	end
 
 
-	def premium_cost(lives, gold, powerups)
-		PREMIUM_PACKAGE_FACTOR * lives + PREMIUM_RATE * (gold + powerups)
+	def premium_cost(lives, package)
+		PREMIUM_PACKAGE_FACTOR * lives + PREMIUM_RATE * (package.gold + package.powerups)
 	end
 
-	def basic_cost(lives, gold, powerups)
-		BASIC_PACKAGE_FACTOR * lives + BASIC_RATE * (gold + powerups)
+	def basic_cost(lives, package)
+		BASIC_PACKAGE_FACTOR * lives + BASIC_RATE * (package.gold + package.powerups)
 	end
 
 end
+
+
+class Package
+	attr_reader :gold, :powerups
+
+	def initialize(gold, powerups)
+		@gold = gold
+		@powerups = powerups
+	end
+end
+
+package1 = Package.new(gold = 10, powerups = 2)
+game1 = GamePackageCalculator.new
+p game1.calculate_cost(package1) # 15.0
+p game1.calculate_cost(package1, true) # 30.3
