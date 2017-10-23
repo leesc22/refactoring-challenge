04b2029d99390e0ddcde29a64a9685178d0abf4a
leesc22 <leesiewching22@gmail.com>
2017-10-23 10:04:46 +0800

    Further refactoring by moving lives into class Package

--- introduce_parameter_object.rb
+++ introduce_parameter_object.rb
@@ -10,46 +10,44 @@ class GamePackageCalculator
 	PREMIUM_PACKAGE_FACTOR = 2.1
 	BASIC_RATE = 1
 	PREMIUM_RATE = 2
-	GOLD_LIFE_RATIO = 0.2
-	POWERUPS_LIFE_RATIO = 0.5
-
 
 	#calculates the cost of a gamer who wants to get X gold, Y powerup and number of lives derived from X gold and Y powerup. 
 	def calculate_cost(package, premium = nil)
-		lives = calculate_lives(package) 
-		
-		if premium
-			premium_cost(lives, package) 
-		else
-			basic_cost(lives, package)
-		end
+		premium ? premium_cost(package) : basic_cost(package)
 	end	
 
 	private 
 
-	def calculate_lives(package)
-		(package.gold * GOLD_LIFE_RATIO ).round(0) + (package.powerups * POWERUPS_LIFE_RATIO).round(0) 
+	def premium_cost(package)
+		PREMIUM_PACKAGE_FACTOR * package.lives + PREMIUM_RATE * (package.gold + package.powerups)
 	end
 
-
-	def premium_cost(lives, package)
-		PREMIUM_PACKAGE_FACTOR * lives + PREMIUM_RATE * (package.gold + package.powerups)
-	end
-
-	def basic_cost(lives, package)
-		BASIC_PACKAGE_FACTOR * lives + BASIC_RATE * (package.gold + package.powerups)
+	def basic_cost(package)
+		BASIC_PACKAGE_FACTOR * package.lives + BASIC_RATE * (package.gold + package.powerups)
 	end
 
 end
 
 
 class Package
-	attr_reader :gold, :powerups
+
+	GOLD_LIFE_RATIO = 0.2
+	POWERUPS_LIFE_RATIO = 0.5
+
+	attr_reader :gold, :powerups, :lives
 
 	def initialize(gold, powerups)
 		@gold = gold
 		@powerups = powerups
+		@lives = calculate_lives
 	end
+
+	private
+
+	def calculate_lives
+		(@gold * GOLD_LIFE_RATIO ).round(0) + (@powerups * POWERUPS_LIFE_RATIO).round(0)
+	end
+	
 end
 
 package1 = Package.new(gold = 10, powerups = 2)
